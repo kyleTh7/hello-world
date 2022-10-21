ef0f8d2f3cc764bc0b70a3c0ea8ad16b9b204d83
kyleTh7 <kyle.thompson7@pcc.edu>
2022-10-20 18:58:50 -0700

    Added main method that prints hello world!

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,12 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * @author Kyle Thompson
+ * @version 2022.20.10
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
