5fbf707afd85129491ca3aa4fc819796229c4d38
kyleTh7 <kyle.thompson7@pcc.edu>
2022-10-20 19:02:03 -0700

    print the date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,5 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
 
 /**
  * @author Kyle Thompson
@@ -8,5 +9,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
